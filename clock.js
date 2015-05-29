821d1974be2294d84fcf45363f4de930736cdd7a
unknown <cwoods@WS-I3-28.WS-ACADBUS.EDU>
2015-05-29 09:47:11 -0700

    final

--- clock.js
+++ clock.js
@@ -1,7 +1,7 @@
 
 
 var clock = function (){
-var d = new Date();
+	var d = new Date();
 	document.getElementById("demo").innerHTML = d.toLocaleTimeString();
 	var t = setTimeout(function(){clock()}, 500);
 };
